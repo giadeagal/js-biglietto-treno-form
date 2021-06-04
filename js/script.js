b333c9ec8758daa0dd4273572d18477505159072
Gianluca <gl.galassini@gmail.com>
2021-06-04 16:08:03 +0200

    variabili input

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 var genera = document.getElementById("genera");
 var annulla = document.getElementById("annulla");
+var fullName = document.getElementById("fullname").value;
+var km = document.getElementById("km").value;
+var age = document.getElementById("age").value;
+
 
 genera.addEventListener("click", function() {
     document.getElementById("loadingGif").classList.add("d-none");
