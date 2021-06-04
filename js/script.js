0aaff880cc43defdfdac3813dbe835f9db0d7137
Gianluca <gl.galassini@gmail.com>
2021-06-04 16:56:30 +0200

    nome, offerta, carrozza

--- js/script.js
+++ js/script.js
@@ -7,8 +7,14 @@ var age = document.getElementById("age").value;
 
 genera.addEventListener("click", function() {
     document.getElementById("loadingGif").classList.add("d-none");
-    document.getElementById("biglietto").classList.remove("d-none")
-    
+    document.getElementById("biglietto").classList.remove("d-none");
+    document.getElementById("tname").innerHTML = fullName;
+    if (age == "minorenne") {
+        document.getElementById("tdiscount").innerHTML = "Sconto Junior"; 
+    } else if (age == "over") {
+        document.getElementById("tdiscount").innerHTML = "Sconto Senior"; 
+    };
+    document.getElementById("tcarriage").innerHTML = Math.floor(Math.random()*9)+1;
 });
 
 annulla.addEventListener("click", function() {
