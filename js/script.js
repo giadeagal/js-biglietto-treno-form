87b01511700e539f752523aff54d660a92734b21
Gianluca <gl.galassini@gmail.com>
2021-06-04 16:35:23 +0200

    inserisco tabella

--- js/script.js
+++ js/script.js
@@ -7,8 +7,12 @@ var age = document.getElementById("age").value;
 
 genera.addEventListener("click", function() {
     document.getElementById("loadingGif").classList.add("d-none");
+    document.getElementById("biglietto").classList.remove("d-none")
+    
 });
 
 annulla.addEventListener("click", function() {
     document.getElementById("loadingGif").classList.remove("d-none");
+    document.getElementById("biglietto").classList.add("d-none");
+
 });
