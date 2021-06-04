5a9814f690539aec3e0a3e4ac47f81b428b75a5f
Gianluca <gl.galassini@gmail.com>
2021-06-04 17:17:10 +0200

    sposto le variabili

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
 var genera = document.getElementById("genera");
 var annulla = document.getElementById("annulla");
-var fullName = document.getElementById("fullname").value;
-var km = document.getElementById("km").value;
-var age = document.getElementById("age").value;
-
 
 genera.addEventListener("click", function() {
     document.getElementById("loadingGif").classList.add("d-none");
     document.getElementById("biglietto").classList.remove("d-none");
+
+    var fullName = document.getElementById("fullname").value;
+    var km = document.getElementById("km").value;
+    var age = document.getElementById("age").value;
+
     document.getElementById("tname").innerHTML = fullName;
     document.getElementById("tprice").innerHTML = km * 0.21 + " €";
+
     if (age == "minorenne") {
         document.getElementById("tdiscount").innerHTML = "Sconto Junior"; 
         document.getElementById("tprice").innerHTML = km * 0.21 * 80 / 100 + " €";
@@ -17,6 +19,7 @@ genera.addEventListener("click", function() {
         document.getElementById("tdiscount").innerHTML = "Sconto Senior";
         document.getElementById("tprice").innerHTML = km * 0.21 * 60 / 100 + " €"; 
     };
+    
     document.getElementById("tcarriage").innerHTML = Math.floor(Math.random()*9)+1;
     document.getElementById("tcode").innerHTML = Math.floor(Math.random() * 100001);
 });
